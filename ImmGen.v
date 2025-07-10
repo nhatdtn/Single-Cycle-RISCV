b09518d0e2c9c8534639c3e96b29b4cd99818a0d
nhatdtn <135865937+nhatdtn@users.noreply.github.com>
2025-07-10 22:50:01 +0700

    Update ImmGen.v

--- ImmGen.v
+++ ImmGen.v
@@ -1,25 +1,25 @@
 module ImmGen (
-    input [31:0] inst,
-    output reg [31:0] imm
+    input [31:0] instr,
+	output reg [31:0] immValue
 );
     always @(*) begin
-	$display("ImmGen: Cycle=%0d, inst=%h, imm=%h", $time/10, inst, imm);
-        case (inst[6:0]) // Opcode
+	    $display("ImmGen: Cycle=%0d, instr=%h, immValue=%h", $time/10, instr, immValue);
+        case (instr[6:0]) // Opcode
             7'b0010011: // I-type (ADDI, etc.)
-                imm = {{20{inst[31]}}, inst[31:20]};
+                immValue = {{20{instr[31]}}, instr[31:20]};
             7'b0000011: // Load (LW)
-                imm = {{20{inst[31]}}, inst[31:20]};
+                immValue = {{20{instr[31]}}, instr[31:20]};
             7'b0100011: // Store (SW)
-                imm = {{20{inst[31]}}, inst[31:25], inst[11:7]};
+                immValue = {{20{instr[31]}}, instr[31:25], instr[11:7]};
             7'b1100011: // Branch (BEQ, etc.)
-                imm = {{19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
+                immValue = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
             7'b1101111: // JAL
-                imm = {{11{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
+                immValue = {{11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0};
             7'b0110111: // LUI
-                imm = {inst[31:12], 12'h000};
+                immValue = {instr[31:12], 12'h000};
             7'b0010111: // AUIPC
-                imm = {inst[31:12], 12'h000};
-            default: imm = 32'h00000000;
+                immValue = {instr[31:12], 12'h000};
+            default: immValue = 32'h00000000;
         endcase
     end
 endmodule
