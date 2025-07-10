83646122a2c1908ce657b3c0a54392e05daeee40
nhatdtn <135865937+nhatdtn@users.noreply.github.com>
2025-07-10 22:39:57 +0700

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,14 +1,14 @@
 module PC (
     input clk,
     input rst_n,
-    input [31:0] pc_in,
-    input pc_write,
-    output reg [31:0] pc_out
+    input [31:0] pcValueIn,
+    input pcWrite,
+    output reg [31:0] pcValueOut
 );
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
-            pc_out <= 32'h00000000;
-        else if (pc_write)
-            pc_out <= pc_in;
+            pcValueOut <= 32'd0;
+        else if (pcWrite)
+            pcValueOut <= pcValueIn;
     end
 endmodule
