25a40116e8c2c6457e70ecc0b2628fb0f4783b0b
nhatdtn <135865937+nhatdtn@users.noreply.github.com>
2025-07-10 22:34:31 +0700

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,26 +1,26 @@
 module ALU (
-    input [31:0] operand1,
-    input [31:0] operand2,
+    input [31:0] value1,
+    input [31:0] value2,
     input [3:0] alu_op,
-    output reg [31:0] result,
+    output reg [31:0] finalValue,
     output reg zero
 );
     always @(*) begin
-	$display("ALU: Cycle=%0d, op1=%h, op2=%h, alu_op=%b, result=%h",
-                 $time/10, operand1, operand2, alu_op, result);
+	$display("ALU: Cycle=%0d, op1=%h, op2=%h, alu_op=%b, finalValue=%h",
+                 $time/10, value1, value2, alu_op, finalValue);
         case (alu_op)
-            4'b0000: result = operand1 + operand2; // ADD
-            4'b0001: result = operand1 - operand2; // SUB
-            4'b0010: result = operand1 & operand2; // AND
-            4'b0011: result = operand1 | operand2; // OR
-            4'b0100: result = operand1 ^ operand2; // XOR
-            4'b0101: result = operand1 << operand2[4:0]; // SLL
-            4'b0110: result = operand1 >> operand2[4:0]; // SRL
-            4'b0111: result = $signed(operand1) >>> operand2[4:0]; // SRA
-            4'b1000: result = ($signed(operand1) < $signed(operand2)) ? 32'h1 : 32'h0; // SLT
-            4'b1001: result = (operand1 < operand2) ? 32'h1 : 32'h0; // SLTU
-            default: result = 32'h00000000;
+            4'b0000: finalValue = value1 + value2; // ADD
+            4'b0001: finalValue = value1 - value2; // SUB
+            4'b0010: finalValue = value1 & value2; // AND
+            4'b0011: finalValue = value | value2; // OR
+            4'b0100: finalValue = value1 ^ value2; // XOR
+            4'b0101: finalValue = value1 << value2[4:0]; // SLL
+            4'b0110: finalValue = value1 >> value2[4:0]; // SRL
+            4'b0111: finalValue = $signed(value1) >>> value2[4:0]; // SRA
+            4'b1000: finalValue = ($signed(value1) < $signed(value2)) ? 32'h1 : 32'h0; // SLT
+            4'b1001: finalValue = (value1 < value2) ? 32'h1 : 32'h0; // SLTU
+            default: finalValue = 32'h00000000;
         endcase
-        zero = (result == 32'h00000000);
+        zero = (finalValue == 32'h00000000);
     end
 endmodule
